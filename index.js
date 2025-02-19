64cfcde86cd5a27218fbc7d5795450ecbd094a0f
SHADABALIGITHUB <shad46243@gmail.com>
2025-02-19 22:26:16 +0530

    Added A >env file

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import axios from "axios";
-
+import { config } from "dotenv";
+config();
 const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 // TODO: Replace the values below with your own before running this file.
-const yourUsername = "zakir";
-const yourPassword = "zakir@123";
-const yourAPIKey = "9609997e-0b36-4403-bfc6-d6e8111350dc";
-const yourBearerToken = "59face65-72ee-40fc-8e34-0ed95ad1433d";
+
+const yourUsername = process.env.YOUR_USERNAME;
+const yourPassword = process.env.YOUR_PASSWORD;
+const yourAPIKey = process.env.YOUR_API_KEY;
+const yourBearerToken = process.env.YOUR_BEARER_TOKEN;
 
 app.get("/", (req, res) => {
     res.render("index.ejs", { content: "API Response." });
